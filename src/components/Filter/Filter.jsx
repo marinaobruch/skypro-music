9db656ea91a98a709b42a2b162d10a8591594c0b
Marina Obruch <marina_obruch@mail.ru>
2023-08-20 13:10:07 +0500

    Initiate version of popup

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Filter.css";
 import FilterItem from "../FilterItem/FilterItem";
 
 export default function Filter() {
+  const [filter, setFilter] = useState(false);
+
+  const handleFilter = () => {
+    setFilter(!filter);
+  };
+
   return (
     <div className="centerblock__filter filter">
       <div className="filter__title">Искать по:</div>
-      <FilterItem value="исполнителю" />
-      <FilterItem value="году выпуска" />
-      <FilterItem value="жанру" />
+      <FilterItem
+        value="исполнителю"
+        onFilterClick={() => handleFilter()}
+        filter={filter}
+      />
+      <FilterItem
+        value="году выпуска"
+        onFilterClick={() => handleFilter()}
+        filter={filter}
+      />
+      <FilterItem
+        value="жанру"
+        onFilterClick={() => handleFilter()}
+        filter={filter}
+      />
     </div>
   );
 }
--- src/components/FilterItem/FilterItem.jsx
+++ src/components/FilterItem/FilterItem.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import "./FilterItem.css";
 
-export default function FilterItem({ value }) {
-  return <div className="filter__button button-author _btn-text">{value}</div>;
+export default function FilterItem({ value, onFilterClick, filter }) {
+  return (
+    <div>
+      <div
+        className="filter__button _btn-text"
+        onClick={onFilterClick}
+      >
+        {value}
+      </div>
+      {filter ? (
+        <div className="filter__form">
+          <p className="filter__item">111111</p>
+          <p className="filter__item">111111</p>
+          <p className="filter__item">111111</p>
+          <p className="filter__item">111111</p>
+          <p className="filter__item">111111</p>
+        </div>
+      ) : null}
+    </div>
+  );
 }
