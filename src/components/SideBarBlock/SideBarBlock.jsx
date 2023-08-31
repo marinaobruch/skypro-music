db0509861c4e8c3b088d21db8bc603eb9e7b4962
Marina Obruch <marina_obruch@mail.ru>
2023-09-01 00:26:24 +0500

    Added router for categories

--- src/components/SideBarBlock/SideBarBlock.jsx
+++ src/components/SideBarBlock/SideBarBlock.jsx
@@ -1,22 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import * as S from "./SideBarBlock.styles.js";
 import SkeletonSidebar from "../SkeletonSidebar/SkeletonSidebar";
 
 export default function SideBarBlock({ loading, albums }) {
   return (
     <S.SidebarBlock>
-      {albums.map((albom) => (
-        <S.SidebarList key={albom.id}>
+      {albums.map((album) => (
+        <S.SidebarList key={album.id}>
           <S.SidebarItem>
             {loading ? (
               <SkeletonSidebar />
             ) : (
-              <S.SidebarLink href={albom.playlistImg}>
-                <S.SidebarImgItem
-                  src={albom.playlistImg}
-                  alt={albom.playlistAlt}
-                />
-              </S.SidebarLink>
+              <Link to={`category/${album.id}`}>
+                <S.SidebarLink href={album.playlistImg}>
+                  <S.SidebarImgItem
+                    src={album.playlistImg}
+                    alt={album.playlistAlt}
+                  />
+                </S.SidebarLink>
+              </Link>
             )}
           </S.SidebarItem>
         </S.SidebarList>
--- src/pages/categories/CategoriesPage.jsx
+++ src/pages/categories/CategoriesPage.jsx
@@ -1,7 +1,16 @@
+import { useParams } from "react-router-dom";
+import { ALBUMS } from "../../constants.js";
+
 export const CategoriesPage = () => {
+  const params = useParams();
+
+  const album = ALBUMS.find((album) => album.id === Number(params.id));
+  console.log(album);
+
   return (
     <section>
-      <h1>Category</h1>
+      <h1>Category {album.id}</h1>
+      <h3>{album.playlistName}</h3>
     </section>
   );
 };
